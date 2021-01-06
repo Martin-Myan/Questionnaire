2600c930b0a28b1bf47c95866cb32e9af4c2f7ad
Mrdo1999 <martirosyanmrd@gmail.com>
2021-01-06 18:10:19 +0400

    finish code review

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Header = ({ nextPage }) => {
-  return (
-    <header>
-      <h1>Questionnaire</h1>
-      <h2>Question {nextPage}</h2>
-    </header>
-  );
-};
+const Header = ({ nextPage }) => (
+  <header>
+    <h1>Questionnaire</h1>
+    <h2>Question {nextPage}</h2>
+  </header>
+);
 
 Header.defaultProps = {
   nextPage: 1,
